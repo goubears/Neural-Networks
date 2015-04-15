fb184fc0cc33f5117a2821c75190a36635f3d71c
Aams1000 <amillers@bowdoin.edu>
2015-04-14 20:29:55 -0400

    Bug fixed! Looks like it can't read in 8x8 files now?

--- Path.java
+++ Path.java
@@ -43,7 +43,7 @@ public class Path{
 	    	potentialWeight = weightGenerator.nextDouble();
 	    }
 
-	    weight = INITIAL_WEIGHT_LIMIT*2 - potentialWeight;
+	    weight = INITIAL_WEIGHT_LIMIT - potentialWeight;
 	    //System.out.println("Weight: " + weight);
 	}
 
